9f428680a49a2d90d29045ac4be6f4e61c2ca470
capitalch <capitalch@gmail.com>
2019-07-11 21:06:38 +0530

    init

--- adam1/adam1.py
+++ adam1/adam1.py
@@ -1,3 +1,16 @@
 print('Hello world')
 myList = ['a','b','c']
 print (myList)
+i=100
+if i==0:
+    print('i is zero')
+elif i==1:
+    print('i is one')
+else:
+    print('i is' + str(i))
+
+myList = ['a', 'bsdd','cdd']
+for x in myList:
+    print(x,len(x))
+for y in range(5, 15):
+    print(y)
