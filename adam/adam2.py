22e6fdbbcf63e26aed41784540f01925a9624f87
capitalch <capitalch@gmail.com>
2020-01-16 18:19:14 +0530

    init

--- adam/adam2.py
+++ adam/adam2.py
@@ -27,50 +27,68 @@ d1 = [
     {
         "accName": "Conveyance",
         "amount": 100,
-        "dc": "d",
+        "dc": "D",
         "remarks": "shop to office"
     },
     {
         "accName": "Cash A/c",
         "amount": 700,
-        "dc": "c",
+        "dc": "C",
         "remarks": "shop to office"
     },
     {
         "accName": "Showroom Exp",
         "amount": 500,
-        "dc": "d",
+        "dc": "D",
         "remarks": "Repairs of items"
     },
     {
         "accName": "Office exp",
         "amount": 100,
-        "dc": "d",
+        "dc": "C",
         "remarks": "Furniture repairs"
     },
     {
         "accName": "Office exp",
         "amount": 200,
-        "dc": "d",
+        "dc": "D",
         "remarks": "Mix repairs"
     },
     {
         "accName": "Cash A/c",
         "amount": 200,
-        "dc": "c",
+        "dc": "C",
+        "remarks": "paid"
+    },
+    {
+        "accName": "Cash A/c",
+        "amount": -1200.23,
+        "dc": "O",
         "remarks": "paid"
     }
 ]
 df = pd.DataFrame(d1)
-pivot = pd.pivot_table(df,index=["accName"],columns="dc", 
-                       values = "amount", aggfunc=np.sum, fill_value=0)
+pivot = pd.pivot_table(df, index=["accName"], columns="dc",
+                       values="amount", aggfunc=np.sum, fill_value=0).reindex(columns=['O', 'D', 'C'])
 pivot.rename(
-  columns={
-    'c' : 'Credit',
-    'd' : 'Debit'
-  },
-  inplace=True
+    columns={
+        'D': 'Debit',
+        'C': 'Credit',
+        'O': 'Opening'
+    },
+    inplace=True
 )
+
+pivot['Closing'] = pivot['Opening'] + pivot['Debit'] - pivot['Credit']
+pivot['clos_dc'] = pivot['Closing'].apply(lambda x: 'Dr' if x >= 0 else 'Cr')
+pivot['op_dc'] = pivot['Opening'].apply(lambda x: 'Dr' if x >= 0 else 'Cr')
+# pivot.reindex(
+#     columns=['O','op_dc', 'D', 'C', 'clos_dc']
+# )
+# pivot.rename(
+#     columns={
+#         'cl_d_c': 'c'
+#     })
 json = pivot.to_json(orient='table')
 print(pivot)
 print(json)
