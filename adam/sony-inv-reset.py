ed3984073324bd576fab3839d597e69ee460d116
capitalch <capitalch@gmail.com>
2020-01-30 16:48:49 +0530

    init

--- adam/sony-inv-reset.py
+++ adam/sony-inv-reset.py
@@ -1,17 +1,82 @@
 
 import sqlanydb
-
 connection = None
+
 sql = '''
-update adjustment set remarks = 'adj1' where adj_id = 3
-update adjustment set remarks = 'adj2' where adj_id = 3
-update adjustment set remarks = 'adj3' where adj_id = 3
-update adjustment set remarks = 'adj5' where adj_id = 3
+truncate table tally_details;
+truncate table tally;
+truncate table part;
+CALL sa_reset_identity( 'part', 'DBA', 0 );
 '''
+
+sql1 = '''
+truncate table order_cancellation_details;
+truncate table order_cancellation_all;
+truncate table order_old;
+truncate table order_current;
+truncate table order_main;
+truncate table order_groupa;
+truncate table order_all;'''
+
+sql2 = '''
+truncate table inv_use_details;
+CALL sa_reset_identity( 'inv_use_details', 'DBA', 0 );
+truncate table inv_bill_details;
+truncate table inv_sale_bill;
+truncate table inv_invoice_details;
+CALL sa_reset_identity( 'inv_invoice_details', 'DBA', 0 );
+truncate table inv_purchase_invoice;
+CALL sa_reset_identity( 'inv_purchase_invoice', 'DBA', 0 );'''
+
+sql3='''
+truncate table inv_physical_stock_hist_details;
+truncate table inv_physical_stock_hist;
+truncate table inv_physical_stock;
+truncate table inv_other_charges;
+CALL sa_reset_identity( 'inv_other_charges', 'DBA', 0 );
+truncate table inv_loan_details;
+CALL sa_reset_identity( 'inv_loan_details', 'DBA', 0 );
+truncate table inv_loan_tran;
+CALL sa_reset_identity( 'inv_loan_tran', 'DBA', 0 );'''
+
+sql4='''
+truncate table inv_direct_adjust;
+CALL sa_reset_identity( 'inv_direct_adjust', 'DBA', 0 );
+truncate table book_main;
+truncate table book_cancellation_details;
+truncate table book_cancellation_all;
+truncate table book_all_details;
+truncate table book_all;
+truncate table adjustment;
+CALL sa_reset_identity( 'adjustment', 'DBA', 0 );'''
+
+sql5='''
+update inv_main set
+job_booking = 0,
+loan_bal= 0,
+cust_pending= 0,
+cust_cancelled= 0,
+cust_booktodate= 0,
+cust_billtodate= 0,
+supp_pending= 0,
+supp_rectodate= 0,
+supp_ordtodate= 0,
+supp_cancelled= 0,
+supp_currorder= 0,
+adjust= 0,
+cr= 0,
+db= 0,
+op_bal= 0;'''
+
 try:
     connection = sqlanydb.connect(uid='dba', pwd='sql', eng='server', dbn='sony', host='kushserver' )
     cur = connection.cursor()
-    cur.execute(sql)
+    # cur.execute(sql)
+    cur.execute(sql1)
+    cur.execute(sql2)
+    cur.execute(sql3)
+    cur.execute(sql4)
+    cur.execute(sql5)
     connection.commit()
 except(Exception) as error:
     print('Error', error)
@@ -21,4 +86,6 @@ finally:
     if connection:
         cur.close()
         connection.close()
-        print('Closed')
+        print('Completed and connection closed')
+
+
