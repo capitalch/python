92aef8565e67d3baba70c8c2fc3db2c80b7ad36d
SushantAgrawal <sagarwal@netwoven.com>
2020-03-07 13:06:11 +0530

    init

---
+++ adam/mydata/bsData.py
@@ -0,0 +1,101 @@
+data1 = [
+        {
+          "accCode": "CurrentAssets",
+          "amount": -70681.16,
+          "children": [
+            16,
+            17
+          ],
+          "id": 15,
+          "parentId": None
+        },
+        {
+          "accCode": "BankAccounts",
+          "amount": -21534,
+          "children": [
+            32,
+            33
+          ],
+          "id": 16,
+          "parentId": 15
+        },
+        {
+          "accCode": "CashInHand",
+          "amount": -49147.16,
+          "children": [
+            31
+          ],
+          "id": 17,
+          "parentId": 15
+        },
+        {
+          "accCode": "IndirectExpences",
+          "amount": 70513.16,
+          "children": [
+            35,
+            36,
+            37
+          ],
+          "id": 25,
+          "parentId": None
+        },
+        {
+          "accCode": "CashAccountShop",
+          "amount": -49147.16,
+          "children": None,
+          "id": 31,
+          "parentId": 17
+        },
+        {
+          "accCode": "AllahabadBankChowringhee",
+          "amount": -34,
+          "children": None,
+          "id": 32,
+          "parentId": 16
+        },
+        {
+          "accCode": "HDFCBankParkStreet",
+          "amount": -21500,
+          "children": None,
+          "id": 33,
+          "parentId": 16
+        },
+        {
+          "accCode": "ShowroomExp",
+          "amount": 341.66,
+          "children": None,
+          "id": 35,
+          "parentId": 25
+        },
+        {
+          "accCode": "OfficeExpences",
+          "amount": 7437.25,
+          "children": None,
+          "id": 36,
+          "parentId": 25
+        },
+        {
+          "accCode": "Telephone",
+          "amount": 62791.25,
+          "children": [
+            38,
+            40
+          ],
+          "id": 37,
+          "parentId": 25
+        },
+        {
+          "accCode": "PH-22280091",
+          "amount": 60378.28,
+          "children": None,
+          "id": 38,
+          "parentId": 37
+        },
+        {
+          "accCode": "PH-22280608",
+          "amount": 2434.97,
+          "children": None,
+          "id": 40,
+          "parentId": 37
+        }
+      ]
