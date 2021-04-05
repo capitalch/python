241ea83981cb8223ad7d3fd636a5b60b8f531898
Sushant Agrawal <capitalch@gmail.com>
2021-04-05 20:55:10 +0530

    init

--- adam/adam7.py
+++ adam/adam7.py
@@ -1,71 +1,48 @@
 import simplejson as json
 import pandas as pd
 import io
+import psycopg2
+from psycopg2.extras import RealDictCursor, DictCursor
+from adam8 import execSql
 
-jDataString = '''[
-{
-    "country": "India",
-    "capital":"New Delhi",
-    "population":"130CR"
-},
-{
-    "country": "Usa",
-    "capital":"Wash",
-    "population":"50CR"
-}]'''
+try:
+    connection = None
+    with open('adam/config.json') as f:
+        cfg = json.load(f)
+    connection = psycopg2.connect(
+        user=cfg["user"], password=cfg["password"], host=cfg["host"], port=cfg["port"], database=cfg["database"])
+    # cursor = connection.cursor(cursor_factory=RealDictCursor)
+    cursor = connection.cursor(cursor_factory=)
+    data = execSql(cursor)
 
-d1 = [
-    {
-        "accName": "Conveyance",
-        "amount": 100,
-        "dc": "D",
-        "remarks": "shop to office"
-    },
-    {
-        "accName": "Cash A/c",
-        "amount": 700,
-        "dc": "C",
-        "remarks": "shop to office"
-    },
-    {
-        "accName": "Showroom Exp",
-        "amount": 500,
-        "dc": "D",
-        "remarks": "Repairs of items"
-    },
-    {
-        "accName": "Office exp",
-        "amount": 100,
-        "dc": "C",
-        "remarks": "Furniture repairs"
-    },
-    {
-        "accName": "Office exp",
-        "amount": 200,
-        "dc": "D",
-        "remarks": "Mix repairs"
-    },
-    {
-        "accName": "Cash A/c",
-        "amount": 200,
-        "dc": "C",
-        "remarks": "paid"
-    },
-    {
-        "accName": "Cash A/c",
-        "amount": 1200.23,
-        "dc": "D",
-        "remarks": "paid"
-    }
-]
+    sheets = list(data[0].keys())    
+    writer = pd.ExcelWriter(r'c:\gst-all-reports.xlsx', engine='xlsxwriter')
 
-df = pd.DataFrame(d1)
-df['test'] = [1,'abc',1,1,1,1,1]
-df.sum(axis=0)
-# del df['dc']
-# print(df[0:3])
-# print (df)
-df.to_excel(r'c:\test.xlsx', sheet_name='mytest', index=False, header=True)
+    for sheet in sheets:
+        df = pd.DataFrame(data[0].get(sheet))
+        print(df)
+        df.to_excel(writer, sheet_name=sheet,
+                 index=False, header=True)
+    writer.save()
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+    if connection:
+        connection.rollback()
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+
+# df=pd.DataFrame(d1)
+# df['test']=[1, 'abc', 1, 1, 1, 1, 1]
+# df.sum(axis = 0)
+# # del df['dc']
+# # print(df[0:3])
+# # print (df)
+# df.to_excel(r'c:\test.xlsx', sheet_name = 'mytest',
+#             index = False, header = True)
 # output = io.BytesIO()
 # writer = pd.ExcelWriter(output, engine='xlsxwriter')
 # df.to_excel(writer, index=False, header=True)
@@ -74,3 +51,61 @@ df.to_excel(r'c:\test.xlsx', sheet_name='mytest', index=False, header=True)
 # str = output.read()
 # output.close()
 # print(str)
+
+
+# jDataString = '''[
+# {
+#     "country": "India",
+#     "capital":"New Delhi",
+#     "population":"130CR"
+# },
+# {
+#     "country": "Usa",
+#     "capital":"Wash",
+#     "population":"50CR"
+# }]'''
+
+# d1 = [
+#     {
+#         "accName": "Conveyance",
+#         "amount": 100,
+#         "dc": "D",
+#         "remarks": "shop to office"
+#     },
+#     {
+#         "accName": "Cash A/c",
+#         "amount": 700,
+#         "dc": "C",
+#         "remarks": "shop to office"
+#     },
+#     {
+#         "accName": "Showroom Exp",
+#         "amount": 500,
+#         "dc": "D",
+#         "remarks": "Repairs of items"
+#     },
+#     {
+#         "accName": "Office exp",
+#         "amount": 100,
+#         "dc": "C",
+#         "remarks": "Furniture repairs"
+#     },
+#     {
+#         "accName": "Office exp",
+#         "amount": 200,
+#         "dc": "D",
+#         "remarks": "Mix repairs"
+#     },
+#     {
+#         "accName": "Cash A/c",
+#         "amount": 200,
+#         "dc": "C",
+#         "remarks": "paid"
+#     },
+#     {
+#         "accName": "Cash A/c",
+#         "amount": 1200.23,
+#         "dc": "D",
+#         "remarks": "paid"
+#     }
+# ]
--- adam/adam8.py
+++ adam/adam8.py
@@ -1,15 +1,11 @@
-import simplejson as json
-import pandas as pd
-import numpy as np
 import psycopg2
-from itertools import repeat
 from psycopg2.extras import RealDictCursor, DictCursor
 
 def execSql(cursor):
     out = None
     sqlString = '''
     set search_path to demounit1;
-    -- GstInputAll (ExtGstTranD only) based on "isInput"
+    -- gst-input-all (ExtGstTranD only) based on "isInput"
     with cte1 as (
 	select  "tranDate", "autoRefNo", "userRefNo", "tranType", "gstin", d."amount" - "cgst" - "sgst" - "igst" as "aggregate", "cgst", "sgst", "igst", d."amount",
             "accName",h."remarks", "dc", "lineRefNo", d."remarks" as "lineRemarks"
@@ -34,7 +30,7 @@ def execSql(cursor):
         
         order by "tranDate", h."id"),
     
-    -- GstOutputAll (ExtGstTrand) based on "isInput"
+    -- gst-output-all (ExtGstTrand) based on "isInput"
     cte2 as (
 	select  "tranDate", "autoRefNo", "userRefNo", "tranType", "gstin", d."amount" - "cgst" - "sgst" - "igst" as "aggregate", "cgst", "sgst", "igst", d."amount",
             "accName",h."remarks", "dc", "lineRefNo", d."remarks" as "lineRemarks"
@@ -59,7 +55,7 @@ def execSql(cursor):
         
         order by "tranDate", h."id"),
     
-    -- GstNetSales (considering only table SalePurchaseDetails)
+    -- gst-output-net-sales (considering only table SalePurchaseDetails)
     cte3 as (
         select  "tranDate", "autoRefNo", "userRefNo", "tranType", 
         (select "gstin" from "ExtGstTranD" where "tranDetailsId" = d."id") as "gstin",
@@ -73,8 +69,6 @@ def execSql(cursor):
                 from "TranH" h
                     join "TranD" d
                         on h."id" = d."tranHeaderId"
-        --             join "ExtGstTranD" e
-        --                 on d."id" = e."tranDetailsId"
                     join "AccM" a
                         on a."id" = d."accId"
                     join "TranTypeM" t
@@ -86,7 +80,6 @@ def execSql(cursor):
                     "sgst" <> 0 or
                     "igst" <> 0) and
                     "tranTypeId" in (4,9) and
-        --			"isInput" = false and
                     "finYearId" = 2021
         --             "finYearId" = %(finYearId)s and
         --             "branchId" = %(branchId)s and
@@ -94,7 +87,7 @@ def execSql(cursor):
             order by "tranDate", h."id"
     ),
 
-    -- GstNetPurchases (considering only table SalePurchaseDetails)
+    -- gst-input-net-purchases (considering only table SalePurchaseDetails)
     cte4 as (
         select  "tranDate", "autoRefNo", "userRefNo", "tranType", 
         (select "gstin" from "ExtGstTranD" where "tranDetailsId" = d."id") as "gstin",
@@ -108,8 +101,6 @@ def execSql(cursor):
                 from "TranH" h
                     join "TranD" d
                         on h."id" = d."tranHeaderId"
-        --             join "ExtGstTranD" e
-        --                 on d."id" = e."tranDetailsId"
                     join "AccM" a
                         on a."id" = d."accId"
                     join "TranTypeM" t
@@ -121,7 +112,6 @@ def execSql(cursor):
                     "sgst" <> 0 or
                     "igst" <> 0) and
                     "tranTypeId" in (5,10) and
-        --			"isInput" = false and
                     "finYearId" = 2021
         --             "finYearId" = %(finYearId)s and
         --             "branchId" = %(branchId)s and
@@ -130,7 +120,7 @@ def execSql(cursor):
         order by "tranDate", h."id"
     ),
 
-    -- gstInputVouchers
+    -- gst-input-vouchers
     cte5 as (
         select  "tranDate", "autoRefNo", "userRefNo", "tranType", 
         "gstin", "rate",
@@ -145,8 +135,6 @@ def execSql(cursor):
                         on a."id" = d."accId"
                     join "TranTypeM" t
                         on t."id" = h."tranTypeId"
-        --			join "SalePurchaseDetails" s
-        --				on d."id" = s."tranDetailsId"
                 where
                     ("cgst" <> 0 or
                     "sgst" <> 0 or
@@ -161,67 +149,12 @@ def execSql(cursor):
         order by "tranDate", h."id"
     )
 	select json_build_object(
-            'gstInputAll', (SELECT json_agg(row_to_json(a)) from cte1 a),
-			'gstOutputAll', (SELECT json_agg(row_to_json(b)) from cte2 b),
-			'gstNetSales', (SELECT json_agg(row_to_json(c)) from cte3 c),
-			'gstNetPurchases', (SELECT json_agg(row_to_json(d)) from cte4 d),
-			'gstInputVouchers', (SELECT json_agg(row_to_json(e)) from cte5 e)
+            'gst-input-all', (SELECT json_agg(row_to_json(a)) from cte1 a),
+			'gst-output-all', (SELECT json_agg(row_to_json(b)) from cte2 b),
+            'gst-input-net-purchases', (SELECT json_agg(row_to_json(d)) from cte4 d),
+            'gst-output-net-sales', (SELECT json_agg(row_to_json(c)) from cte3 c), 
+			'gst-input-vouchers', (SELECT json_agg(row_to_json(e)) from cte5 e)
         ) as "jsonResult" '''
     cursor.execute(sqlString)
-    out = cursor.fetchall()
+    out = cursor.fetchone()
     return out
-
-def trialBalance(data):
-    df = pd.DataFrame(data)
-    pivot = pd.pivot_table(df, index=["accCode", "accName", "accType"], columns=["dc"],
-                       values="amount", aggfunc=np.sum, fill_value=0)
-    
-    pivot.rename(
-        columns={
-            'O': 'Opening',
-            'D': 'Debit',
-            'C': 'Credit'
-        },
-        inplace=True
-    )
-    pivot['Closing'] = pivot['Opening'] + pivot['Debit'] - pivot['Credit']
-
-    pivot.loc['Total', 'Closing'] = pivot['Closing'].sum()
-    pivot.loc['Total', 'Debit'] = pivot['Debit'].sum()
-    pivot.loc['Total', 'Credit'] = pivot['Credit'].sum()
-    pivot.loc['Total', 'Opening'] = pivot['Opening'].sum()
-    
-    pivot['Closing_dc'] = pivot['Closing'].apply(lambda x: 'Dr' if x >= 0 else 'Cr')
-    pivot['Closing'] = pivot['Closing'].apply(lambda x: x if x>=0 else -x) # remove minus sign
-    
-    pivot['Opening_dc'] = pivot['Opening'].apply(lambda x: 'Dr' if x >= 0 else 'Cr')
-    pivot['Opening'] = pivot['Opening'].apply(lambda x: x if x>=0 else -x) # remove minus sign
-    
-    pivot = pivot.reindex(columns= ['Opening', 'Opening_dc', 'Debit', 'Credit', 'Closing', 'Closing_dc'])
-    
-    print(pivot)
-    j = pivot.to_json(orient='table')
-    jsonObj = json.loads(j)
-    dt = jsonObj["data"]
-    return dt
-
-try:
-    connection = None
-    with open('adam/config.json') as f:
-        cfg = json.load(f)
-    connection = psycopg2.connect(
-        user=cfg["user"], password=cfg["password"], host=cfg["host"], port=cfg["port"], database=cfg["database"])
-    cursor = connection.cursor(cursor_factory=RealDictCursor)
-    data = execSql(cursor)
-    d = trialBalance(data)
-    # print(d)
-    connection.commit()
-except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-    if connection:
-        connection.rollback()
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")
