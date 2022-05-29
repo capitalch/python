931db5a39bc87dd206d18bc98866a649b2455b03
Sushant Agrawal <capitalch@gmail.com>
2022-05-29 17:14:42 +0530

    init

--- adam/adam1.py
+++ adam/adam1.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import cufflinks as cf
 from IPython.display import display,HTML
 
+tup = [1,2,3,4]
+for x in tup:
+  print(x)
+
 # cf.set_config_file(sharing='public',theme='ggplot',offline=True)
 # df_population = pd.read_csv('population_total.csv')
 # df_population = df_population.dropna()
@@ -14,15 +18,15 @@ from IPython.display import display,HTML
 # df_population.iplot(kind='line',xTitle='Years', yTitle='Population',
 #                     title='Population (1955-2020)')
 
-mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
-}
-df = pd.DataFrame(mydataset)
-json = df.to_json()
-# print(json)
-print(df)
-df = pd.read_csv('data.csv')
-print(df.to_json())
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2]
+# }
+# df = pd.DataFrame(mydataset)
+# json = df.to_json()
+# # print(json)
+# print(df)
+# df = pd.read_csv('data.csv')
+# print(df.to_json())
 # df.plot()
 # plt.show()
--- adam/adam5.py
+++ adam/adam5.py
@@ -5,25 +5,53 @@ import psycopg2
 from itertools import repeat
 from psycopg2.extras import RealDictCursor, DictCursor
 
-sql1 = ''' select "id", "accCode", "accName" from "AccM" where "accLeaf" = 'Y' '''
-sql2 = ''' select "id", "accId", "amount" from "TranD" '''
+sql1 = ''' set search_path to demounit1; select "id", "accCode", "accName" from "AccM" where "accLeaf" = 'Y' '''
+sql2 = ''' set search_path to demounit1; select "id", "accId", "amount" from "TranD" '''
 sqlList = [sql1, sql2]
-sqlTuple = [('sql1',sql1),('sql2',sql2)]
-
+sqlTuple = [('sql1', sql1), ('sql2', sql2)]
+sql3 = '''
+    set search_path to demounit1;
+    select "id", "tranDate", "autoRefNo", "finYearId" from "TranH" where "tranDate" between %(startDate)s and %(endDate)s
+'''
 try:
     connection = None
     with open('config.json') as f:
         cfg = json.load(f)
     connection = psycopg2.connect(
         user=cfg["user"], password=cfg["password"], host=cfg["host"], port=cfg["port"], database=cfg["database"])
-    cursor = connection.cursor(cursor_factory=RealDictCursor)
+
+    cursor = connection.cursor()
+    params = [
+        {'name': 'today', 'startDate': '2022-05-29',
+            'endDate': '2022-05-29', 'tranTypeId': 4},
+        {'name': 'yesterday', 'startDate': '2022-05-28',
+            'endDate': '2022-05-28', 'tranTypeId': 4},
+        {'name': 'thisMonth', 'startDate': '2022-05-01',
+            'endDate': '2022-05-30', 'tranTypeId': 4},
+        {'name': 'lastMonth', 'startDate': '2022-04-01',
+            'endDate': '2022-04-30', 'tranTypeId': 4},
+        {'name': 'ytd', 'startDate': '2022-04-01',
+            'endDate': '2022-05-30', 'tranTypeId': 4},
+    ]
     out = {}
-    for item in sqlTuple:
-        cursor.execute(item[1])
-        out[item[0]] = cursor.fetchall()
-        # out.append(cursor.fetchall())
+    for param in params:
+        cursor.execute(sql3, param)
+        out[param['name']] = cursor.fetchall()
     
     print(out)
+    # x = cursor.mogrify(sql3, [data])
+    # print(x)
+    # cursor.execute(sql3,[data])
+    # out = cursor.fetchall()
+    # print(out)
+
+    # cursor = connection.cursor(cursor_factory=RealDictCursor)
+    # out = {}
+    # for item in sqlTuple:
+    #     cursor.execute(item[1])
+    #     out[item[0]] = cursor.fetchall()
+
+    # print(out)
     connection.commit()
 except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
